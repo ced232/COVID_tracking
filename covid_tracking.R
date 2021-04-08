7953767a7d2225a53145005ff53c788b1e1e6a85
ced232 <57511466+ced232@users.noreply.github.com>
2021-04-07 21:50:36 -0400

    Added outlier detection, grid search for clustering optimization

--- covid_tracking.R
+++ covid_tracking.R
@@ -7,6 +7,7 @@ library(cluster)
 library(dplyr)
 library(factoextra)
 library(ggplot2)
+library(ggsci)
 library(grid)
 library(gridExtra)
 library(NbClust)
@@ -19,8 +20,9 @@ library(viridis)
 # Constants
 # ----------
 
-date_title <- "February 18th"
-customPal <- magma(10)[c(10,8,6)]
+date_title <- "April 7th"
+end_date <- as.Date("2021-4-6")
+customPal <- c(magma(10)[c(10,8,6,4)], "white", "gray60", "gray30", "red")
 
 
 # ----------
@@ -29,15 +31,15 @@ customPal <- magma(10)[c(10,8,6)]
 
 # import time series data:
 
-confirmed_data <- read.csv("confirmed_2021_2_18.csv", stringsAsFactors = FALSE)  %>%
+confirmed_data <- read.csv("~/git/covid_tracking/confirmed_2021_4_7.csv", stringsAsFactors=FALSE)  %>%
     filter(!(Province_State %in% c("American Samoa", "Diamond Princess", "Grand Princess", "Guam", 
-                                 "Northern Mariana Islands", "Puerto Rico", "Virgin Islands"))) %>%
+                                   "Northern Mariana Islands", "Puerto Rico", "Virgin Islands"))) %>%
     select(-UID, -iso2, -iso3, -code3, -FIPS, -Admin2, -Country_Region, -Lat, -Long_, -Combined_Key) %>%
     rename(state = Province_State) %>%
     group_by(state) %>%
     summarise_all(list(sum = sum))
 
-deaths_data <- read.csv("deaths_2021_2_18.csv", stringsAsFactors = FALSE) %>%
+deaths_data <- read.csv("~/git/covid_tracking/deaths_2021_4_7.csv", stringsAsFactors = FALSE) %>%
     filter(!(Province_State %in% c("American Samoa", "Diamond Princess", "Grand Princess", "Guam", 
                                    "Northern Mariana Islands", "Puerto Rico", "Virgin Islands"))) %>%
     select(-UID, -iso2, -iso3, -code3, -FIPS, -Admin2, -Country_Region, -Lat, -Long_, -Combined_Key, -Population) %>%
@@ -56,7 +58,7 @@ for (i in 1:(j-2)) {
 
 #convert raw counts to per capita counts:
 
-state_pop <- read.csv("state_populations.csv", stringsAsFactors = FALSE)
+state_pop <- read.csv("~/git/covid_tracking/state_populations.csv", stringsAsFactors = FALSE)
 
 confirmed_daily_data <- confirmed_data[,c(1,(j+1):(2*j - 2))] %>%
     left_join(state_pop, ., by = "state")
@@ -71,13 +73,124 @@ for (i in 3:j) {
 
 
 # ----------
+# Outliers
+# ----------
+
+# determine number of outlier dates by size of sliding window:
+
+window <- c()
+outliers <- c()
+
+for (i in 5:100) {
+    ws2 <- c()
+    
+    for (j in confirmed_cluster$state) {
+        c <- confirmed_daily_data %>%
+            filter(state == j) %>%
+            select(-state, -pop) %>%
+            gather(date, value) %>%
+            mutate(date = as.Date("2020-1-23"):end_date) %>%
+            mutate(date = as.Date(date, origin = "1970-1-1"))
+        
+        ws <- c()
+        
+        for (k in 1:(nrow(c) - i)) {
+            c_subset <- c$value[k:(k + i - 1)]
+            q3 <- fivenum(c_subset)[4]
+            q1 <- fivenum(c_subset)[2]
+            IQR <- q3 - q1
+            
+            w <- c(which(c_subset > q3 + 1.5*IQR), which(c_subset < q1 - 1.5*IQR))
+            ws <- c(ws, w + k)
+        }
+    }
+    
+    window <- c(window, i)
+    ws2 <- c(ws2, unique(ws))
+    outliers <- c(outliers, length(ws2))
+    
+    print(i)
+    
+}
+
+outlier_df <- data.frame(window, outliers)
+
+# visualize relationship between window size and number of outlier dates:
+
+outlier_plot <- outlier_df %>%
+    ggplot(aes(x = window, y = outliers)) +
+    ggtitle("\nWindow Size for Outlier Detection",
+            subtitle = paste0(date_title, "\n")) +
+    geom_line(color = plasma(10)[4]) +
+    geom_smooth(color = "white", size = .8, span = .1, se = FALSE) +
+    scale_x_continuous(name = "\nwindow size\n",
+                       limits = c(0, 100),
+                       breaks = seq(0, 100, 20)) +
+    scale_y_continuous(name = "dates \nwith \noutliers ",
+                       limits = c(0, 100),
+                       breaks = seq(0, 100, 25)) +
+    geom_hline(yintercept = 0, color = "white", size = .8) +
+    geom_vline(xintercept = 0, color = "white", size = .8) +
+    theme(panel.grid.minor.y = element_blank(),
+          panel.grid.minor.x = element_line(color = "gray30"),
+          panel.grid.major.y = element_line(color = "gray40"),
+          panel.grid.major.x = element_line(color = "gray50"),
+          text = element_text(color = "white", family = "Avenir"), 
+          strip.text = element_text(color = "white", family = "Avenir Black", size = 12, hjust = 0),
+          axis.text = element_text(color = "white"),
+          axis.title.y = element_text(size = 10, angle = 0, vjust = .5, hjust = 1),
+          plot.title = element_text(family = "Avenir Black", hjust = .5, size = 14), 
+          plot.subtitle = element_text(family = "Avenir", hjust = .5, size = 10),
+          panel.background = element_rect(fill = "black"),
+          #panel.border = element_rect(fill = NA, color = "white", size = 1),
+          plot.background = element_rect(fill = "black", color = "black"), 
+          legend.title = element_text(size = 10),
+          legend.background = element_rect(fill = "black"), 
+          legend.key = element_rect(fill = "black"),
+          legend.key.size = unit(.6, "cm"),
+          legend.text = element_text(size = 8),
+          legend.position = "none",
+          plot.margin = unit(c(5.5, 33, 5.5, 5.5), "pt"))
+
+outlier_plot
+
+# selected threshold, determine outlier dates with that threshold
+
+i <- 12
+
+for (j in confirmed_cluster$state) {
+    c <- confirmed_daily_data %>%
+        filter(state == j) %>%
+        select(-state, -pop) %>%
+        gather(date, value) %>%
+        mutate(date = as.Date("2020-1-23"):as.Date("2021-4-6")) %>%
+        mutate(date = as.Date(date, origin = "1970-1-1"))
+    ws <- c()
+    
+    for (k in 1:(nrow(c) - i)) {
+        c_subset <- c$value[k:(k + i - 1)]
+        q3 <- fivenum(c_subset)[4]
+        q1 <- fivenum(c_subset)[2]
+        IQR <- q3 - q1
+        
+        w <- c(which(c_subset > q3 + 1.5*IQR), which(c_subset < q1 - 1.5*IQR))
+        ws <- c(ws, w + k)
+    }
+}
+
+ws2 <- unique(ws)
+
+outlier_dates <- c$date[ws2]
+
+
+# ----------
 # Principal Component Analysis
 # ----------
 
 # run PCA:
 
 set.seed(125)
-confirmed_prcomp <- confirmed_daily_data %>%
+confirmed_prcomp <- confirmed_daily_data[,-(ws2 + 2)] %>%
     select(-state, -pop) %>%
     prcomp()
 
@@ -86,12 +199,18 @@ confirmed_pca <- data.frame(state = confirmed_daily_data$state,
                             PC2 = confirmed_prcomp$x[, 2],
                             PC3 = confirmed_prcomp$x[, 3],
                             PC4 = confirmed_prcomp$x[, 4],
-                            PC5 = confirmed_prcomp$x[, 5]) %>%
+                            PC5 = confirmed_prcomp$x[, 5],
+                            PC6 = confirmed_prcomp$x[, 6],
+                            PC7 = confirmed_prcomp$x[, 7],
+                            PC8 = confirmed_prcomp$x[, 8]) %>%
     mutate(PC1 = scale(PC1)) %>%
     mutate(PC2 = scale(PC2)) %>%
     mutate(PC3 = scale(PC3)) %>%
     mutate(PC4 = scale(PC4)) %>%
-    mutate(PC5 = scale(PC5)) 
+    mutate(PC5 = scale(PC5)) %>%
+    mutate(PC6 = scale(PC6)) %>%
+    mutate(PC7 = scale(PC7)) %>%
+    mutate(PC8 = scale(PC8))
 
 # visualize cumulative variance:
 
@@ -103,11 +222,11 @@ var_plot <- ggplot(pca_var, aes(x = PC, y = var)) +
             subtitle = paste0(date_title, "\n")) +
     geom_line(color = plasma(10)[4], size = .8) +
     scale_x_continuous(name = "\nPC\n",
-                       limits = c(0,8),
-                       breaks = seq(0,8,2)) +
+                       limits = c(0, 8),
+                       breaks = seq(0, 8, 2)) +
     scale_y_continuous(name = "cumulative \nproportion \nof variance ",
-                       limits = c(0,1),
-                       breaks = seq(0,1,.25)) +
+                       limits = c(0, 1),
+                       breaks = seq(0, 1, .25)) +
     geom_hline(yintercept = 0, color = "white", size = .8) +
     geom_vline(xintercept = 0, color = "white", size = .8) +
     theme(panel.grid.minor.y = element_blank(),
@@ -136,7 +255,7 @@ var_plot
 # visualize PC rotations:
 
 pc_rotations <- as.data.frame(confirmed_prcomp$rotation[,1:8]) %>%
-    mutate(date = as.Date("2020-1-23"):as.Date("2021-2-17")) %>%
+    mutate(date = as.Date("2020-1-23"):(end_date - length(ws2))) %>%
     mutate(date = as.Date(date, origin = "1970-1-1")) %>%
     gather(pc, value, -date)
 
@@ -148,23 +267,23 @@ pc_rotations_plot <- pc_rotations %>%
     ggtitle("\nPrinciple Component Rotations",
             subtitle = paste0(date_title, "\n")) +
     geom_tile(aes(fill = value)) +
-    scale_fill_gradientn(name = "rotation", limits = c(-rlim, rlim), 
+    scale_fill_gradientn(name = "Rotation", limits = c(-rlim, rlim), 
                          colours = c("#B2182B","White","#244999")) +
     scale_y_discrete(name = "principal \ncomponent ", 
                      limits = rev(names(as.data.frame(confirmed_prcomp$rotation[,1:8])))) +
-    scale_x_date(name = "\n\n", date_breaks = "2 months", date_labels = "%B") +
+    scale_x_date(name = "\n\n", date_breaks = "2 months", date_labels = "%b") +
     theme_minimal() +
     theme(panel.grid.minor.x = element_blank(),
-        text = element_text(color = "white", family = "Avenir"), 
-        axis.text = element_text(color = "white"),
-        axis.title.y = element_text(angle = 0, vjust = .5, hjust = 1),
-        plot.title = element_text(family = "Avenir Black", hjust = .5, size = 14), 
-        plot.subtitle = element_text(family = "Avenir", hjust = .5, size = 10),
-        plot.background = element_rect(fill = "black", color = "black"), 
-        legend.background = element_rect(fill = "black"), 
-        legend.key = element_rect(fill = "black"),
-        legend.key.size = unit(.6, "cm"),
-        legend.text = element_text(size = 8))
+          text = element_text(color = "white", family = "Avenir"), 
+          axis.text = element_text(color = "white"),
+          axis.title.y = element_text(angle = 0, vjust = .5, hjust = 1),
+          plot.title = element_text(family = "Avenir Black", hjust = .5, size = 14), 
+          plot.subtitle = element_text(family = "Avenir", hjust = .5, size = 10),
+          plot.background = element_rect(fill = "black", color = "black"), 
+          legend.background = element_rect(fill = "black"), 
+          legend.key = element_rect(fill = "black"),
+          legend.key.size = unit(.6, "cm"),
+          legend.text = element_text(size = 8))
 
 pc_rotations_plot
 
@@ -173,24 +292,100 @@ pc_rotations_plot
 # Clustering
 # ----------
 
-# determine ideal k value:
+# grid search for ideal k and number of PC's to include:
 
-nb <- NbClust(confirmed_pca[,c(2:6)], distance = "euclidean", min.nc = 2,
-              max.nc = 20, method = "complete", index ="all")
-fviz_nbclust(nb) + theme_minimal()
+pcs_col <- c()
+k_val_col <- c()
+error <- c()
 
-k_val <- 3
+for (pcs in 2:8) {
+    for (k_val in 2:6) {
+        set.seed(125)
+        k <- kmeans(confirmed_pca[,c(2:(pcs+1))], k_val)
+        cluster <- factor(k$cluster)
+        confirmed_cluster <- cbind(confirmed_pca, cluster)
+        
+        cluster_sd <- confirmed_cluster %>%
+            select(state, cluster) %>%
+            left_join(., confirmed_daily_data, by = "state") %>%
+            select(-state, -pop) %>%
+            group_by(cluster) %>%
+            summarise_all(list(sd = sd)) %>%
+            gather(date, cases, -cluster) %>%
+            mutate(cases = 1000000*cases) %>%
+            rowwise() %>%
+            mutate(date = gsub("X", "", date)) %>%
+            mutate(date = gsub("_sum.1_sd", "", date)) %>%
+            mutate(month = strsplit(date, ".", fixed = TRUE)[[1]][1]) %>%
+            mutate(day = strsplit(date, ".", fixed = TRUE)[[1]][2]) %>%
+            mutate(year = strsplit(date, ".", fixed = TRUE)[[1]][3]) %>%
+            mutate(date = as.Date(paste0("20", year, "-", month, "-", day))) %>%
+            select(-year, -month, -day) %>%
+            filter(!(is.na(cases))) %>%
+            group_by(date) %>%
+            mutate(cluster = NA) %>%
+            summarise_all(list(mean = mean)) %>%
+            select(date, cases_mean) %>%
+            summarise(sum(cases_mean))
+        
+        pcs_col <- c(pcs_col, pcs)
+        k_val_col <- c(k_val_col, k_val)
+        error <- c(error, as.numeric(cluster_sd))
+    }
+}
 
-# perform k-clustering:
+grid_df <- data.frame(pcs = pcs_col, k_val = k_val_col, error)
+
+# selected values:
+selected <- c(4, 6)
+
+grid_plot <- grid_df %>%
+    mutate(error = round(error)) %>%
+    ggplot(aes(x = pcs, y = k_val)) +
+    ggtitle("\nGrid Search Clustering Optimization",
+            subtitle = paste0(date_title, "\n")) +
+    geom_tile(aes(fill = error)) +
+    geom_text(aes(label = error)) +
+    geom_segment(x = selected[1] - .5, xend = selected[1] + .5, 
+                 y = selected[2] + .5, yend = selected[2] + .5,
+                 color = "white", size = .8) +
+    geom_segment(x = selected[1] - .5, xend = selected[1] + .5, 
+                 y = selected[2] - .5, yend = selected[2] - .5,
+                 color = "white", size = .8) +
+    geom_segment(x = selected[1] + .5, xend = selected[1] + .5, 
+                 y = selected[2] - .5, yend = selected[2] + .5,
+                 color = "white", size = .8) +
+    geom_segment(x = selected[1] - .5, xend = selected[1] - .5, 
+                 y = selected[2] - .5, yend = selected[2] + .5,
+                 color = "white", size = .8) +
+    scale_y_continuous(name = "number \nof \nclusters ",
+                       breaks = seq(2,6,2)) +
+    scale_x_continuous(name = "\nnumber of PC's included\n") +
+    scale_fill_gradientn(name = "Mean\nIntra-cluster\nDeviance",
+                         colours = c(plasma(10)[4], "White")) +
+    theme_minimal() +
+    theme(panel.grid.minor = element_blank(),
+          text = element_text(color = "white", family = "Avenir"), 
+          axis.text = element_text(color = "white"),
+          axis.title.y = element_text(angle = 0, vjust = .5, hjust = 1),
+          plot.title = element_text(family = "Avenir Black", hjust = .5, size = 14), 
+          plot.subtitle = element_text(family = "Avenir", hjust = .5, size = 10),
+          plot.background = element_rect(fill = "black", color = "black"), 
+          legend.background = element_rect(fill = "black"), 
+          legend.key = element_rect(fill = "black"),
+          legend.key.size = unit(.6, "cm"),
+          legend.text = element_text(size = 8))
+
+grid_plot
+
+# validate using silhouettes:
 
 set.seed(125)
-k <- kmeans(confirmed_pca[,c(2:6)], k_val)
+k <- kmeans(confirmed_pca[,c(2:(selected[1]+1))], selected[2])
 cluster <- factor(k$cluster)
 confirmed_cluster <- cbind(confirmed_pca, cluster) 
 
-# visualize silhouettes:
-
-sil <- silhouette(k$cluster, dist(confirmed_pca[,c(2:6)]))
+sil <- silhouette(k$cluster, dist(confirmed_pca[,c(2:(selected[1]+1))]))
 sort_sil <- sortSilhouette(sil)
 
 sil_df <- as.data.frame(sort_sil[,1:3]) %>%
@@ -310,7 +505,7 @@ metric_labeller <- function(variable,value){
 
 trends_plot <- full_plot %>%
     ggplot(aes(x = date, y = cases)) +
-    ggtitle("\nUS Covid-19 Trends by State Group",
+    ggtitle("\nUS COVID-19 Trends by State Cluster",
             subtitle = date_title) +
     geom_hline(yintercept = 0, size = .5, color = "gray45") +
     geom_vline(xintercept = as.Date("2021-1-1"), size = .8, color = "gray45") +
@@ -318,7 +513,7 @@ trends_plot <- full_plot %>%
     scale_color_manual(name = "Group",
                        values = customPal, 
                        na.translate = FALSE, drop = FALSE) +
-    scale_x_date(name = "", date_breaks = "2 months", date_labels = "%B") +
+    scale_x_date(name = "", date_breaks = "2 months", date_labels = "%b") +
     scale_y_continuous(name = "mean daily \ncases/deaths \nper million ") +
     facet_wrap(~metric, nrow = 2, scales = "free", labeller = metric_labeller) +
     theme_minimal() +
@@ -344,39 +539,6 @@ trends_plot <- full_plot %>%
 
 trends_plot
 
-# visualize mean PC values for each cluster:
-
-pc_vals_by_cluster <- confirmed_cluster %>%
-    group_by(cluster) %>%
-    select(-state) %>%
-    summarise_all(mean) %>%
-    gather(PC, value, -cluster)
-
-r <- range(pc_vals_by_cluster$value)
-rlim <- max(abs(r))
-
-pc_vals_by_cluster %>%
-    mutate(cluster = factor(cluster, levels = 3:1)) %>%
-    ggplot(aes(y = cluster, x = PC)) +
-    ggtitle("\nMean PC Values by Cluster",
-            subtitle = paste0(date_title, "\n")) +
-    geom_tile(color = "black", aes(fill = value)) +
-    scale_y_discrete(name = "cluster ") +
-    scale_x_discrete(name = "\nPrincipal Component\n") +
-    scale_fill_gradientn(name = "Mean\nValue", limits = c(-rlim,rlim),
-                         colours = c("#B2182B","White","#244999")) +
-    theme_minimal() +
-    theme(panel.grid.minor.x = element_blank(),
-        text = element_text(color = "white", family = "Avenir"), 
-        axis.text = element_text(color = "white"),
-        axis.title.y = element_text(angle = 0, vjust = .5, hjust = 1),
-        plot.title = element_text(family = "Avenir Black", hjust = .5, size = 14), 
-        plot.subtitle = element_text(family = "Avenir", hjust = .5, size = 10),
-        plot.background = element_rect(fill = "black", color = "black"), 
-        legend.background = element_rect(fill = "black"), 
-        legend.key = element_rect(fill = "black"),
-        legend.key.size = unit(.6, "cm"),
-        legend.text = element_text(size = 8))
 
 
 
